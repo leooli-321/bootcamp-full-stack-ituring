24bbca4ebccd0c0329c98e9c69d4d3da6352ff05
Leo Oli <leooli_artist@outlook.com>
2022-10-09 01:22:10 -0300

    pratica 3 finalizada

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,13 @@
+let nome = "Leo Oli";
+let email = "leooli_artist@outlook.com";
+let profissao = "CEO";
+let idade = 27;
+let sonho = "Japão";
+
+console.log(
+  nome + ", " + email + ", " + profissao + ", " + idade + ", " + sonho + "."
+);
+
+if (typeof myVar === "string") {
+  console.log("true");
+} else console.log("false");
---
+++ mes-1-logica-de-programacao/tarefa-semana-1/test.js
@@ -0,0 +1,27 @@
+var dados = [
+  (nome = "Leo Oli"),
+  (email = "leooli_artist@outlook.com"),
+  (profissao = "CEO"),
+  (idade = 27),
+  (sonho = "Japão"),
+];
+
+if (typeof nome === "string") {
+  console.log("true");
+} else console.log("false");
+
+if (typeof email === "string") {
+  console.log("true");
+} else console.log("false");
+
+if (typeof profissao === "string") {
+  console.log("true");
+} else console.log("false");
+
+if (typeof idade === "string") {
+  console.log("true");
+} else console.log("false");
+
+if (typeof sonho === "string") {
+  console.log("true");
+} else console.log("false");
