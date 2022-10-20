b8ac9a0a0ca01b7d5dbf898fe199e6a7bbb651c0
Leo Oli <leooli_artist@outlook.com>
2022-10-20 18:37:31 -0300

    terminado até o 5

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
@@ -0,0 +1,7 @@
+function lista() {
+
+    const series = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+    console.log(series);
+}
+
+lista();
---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
@@ -0,0 +1,19 @@
+function lista(series, list) {
+
+    series = [
+        "Breaking Bad",
+        "Game of Thrones",
+        "Stranger Things",
+        "Chernobyl",
+        "Dexter",
+        "The Last Dance",
+        "This is Us",
+    ];
+
+    list = series[5];
+
+    console.log(list);
+    return series;
+}
+
+lista();
